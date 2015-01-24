aaa9f8c7e1868e5ba517af93a49ca73b625ae3ff
mbech <matt.b.bechtel@gmail.com>
2015-01-23 20:22:49 -0800

    Add resouce routes for micropost

--- toy_app/config/routes.rb
+++ toy_app/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :microposts
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   resources :users
+  resources :microposts
   root 'users#index'
 
   # Example of regular route:
