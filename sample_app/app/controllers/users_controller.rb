c287837590e9b0feb4d582e3280f91986746d7ac
mbech <matt.b.bechtel@gmail.com>
2015-02-21 19:13:19 -0800

    Add initial create action to users controller

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(params[:user]) #not final!
+    if @user.save
+      #Handle successful creation
+    else
+      render 'new'
+    end
+  end
 end
