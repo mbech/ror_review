64cb8149623135e132443187f160218045d7f4f0
mbech <matt.b.bechtel@gmail.com>
2015-02-21 20:32:23 -0800

    Add redirect on successful signup

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #Handle successful creation
+      redirect_to @user
     else
       render 'new'
     end
