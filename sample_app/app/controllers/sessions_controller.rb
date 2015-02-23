46c4b7437947d291f1fcd8e0e4be53cf1e7ee2f6
mbech <matt.b.bechtel@gmail.com>
2015-02-22 20:04:23 -0800

    Add session create code, flash on error

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:email])
+      #log the user in then redirect
+    else
+      #create an error message
+      flash[:danger] = 'Invaild email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
