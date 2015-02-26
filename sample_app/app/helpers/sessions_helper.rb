10cc74c4d799c23189c29536ffc199719a6b62ff
mbech <matt.b.bechtel@gmail.com>
2015-02-26 09:37:21 -0800

    Add logout helper for session destroy action

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -8,6 +8,12 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Log out current user
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
